c27d97a2136b13ffde47d2c5a5f193f5c146361e
Code-Binder-Spec <adhimansoor.k@gmail.com>
2026-05-30 08:25:22 +0530

    Understanding error print& C:/Users/adhim/AppData/Local/Programs/Python/Python314/python.exe c:/Users/adhim/OneDrive/Documents/GITHUB_REPO_2/Playground/async/task15.py

---
+++ async/task15.py
@@ -0,0 +1,38 @@
+import asyncio
+
+sem = asyncio.Semaphore(4)
+
+async def car_washing(car,time):
+    async with sem:
+                  print(f"{car} washing started")
+                  await asyncio.sleep(time)
+                  print(f"{car} washing completed")
+
+async def all():
+       results = await asyncio.gather(
+                car_washing("ferrari",4),
+                car_washing("lambhorgini",3),
+                car_washing("porshe",2),
+                car_washing("bmw","none"),
+                car_washing("mustang",5),
+                car_washing("supra",7),
+                car_washing("rolls royce",9),
+                car_washing("buggatti",8),
+                return_exceptions=True
+
+        )
+      
+       for i,result in enumerate(results):
+               if isinstance(result,Exception):
+                       print(f"Task {i+1}. Stauts : failed")
+               else :
+                     print(f"Task {i+1}. Stauts : completed")  
+                        
+       
+        
+    
+asyncio.run(all())
+
+       
+
+
