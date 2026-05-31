0938cb9e2c93a15e1f00c80daf9c477df51b9f38
Code-Binder-Spec <adhimansoor.k@gmail.com>
2026-05-31 13:58:28 +0530

    Async with scraping

--- async/aiohttp/practicing_projects/Scraping_with_async.py
+++ async/aiohttp/practicing_projects/Scraping_with_async.py
@@ -1,23 +1,31 @@
 import asyncio
 import aiohttp
+import json
 from bs4 import BeautifulSoup
+
+with open("quote.json","w",encoding="utf-8") as f:
+                 pass
 
 async def getting_source(url):
         async with aiohttp.ClientSession() as session:
-                     async with session.get(url,timeout=aiohttp.ClientTimeout(total=4)) as response:
+                     async with session.get(url,timeout=aiohttp.ClientTimeout(total=6)) as response:
                                                  return await response.text()
         
 async def extracting(url):
          source = await getting_source(url)
          soup = BeautifulSoup(source,"html.parser")
-         qoutes = soup.select("span.text")
+         quotes = soup.select("span.text")
          authors = soup.select("small.author")
-         for qoute,author in zip(qoutes,authors):
-                 print(f"\n Qoute : {qoute.text} . Author : {author.text}")
-
+         return [{"Quote" : quote.text ,"Author" : author.text } for quote,author in zip (quotes,authors)]
 async def all():
         urls = [f"http://quotes.toscrape.com/page/{i}/" for i in range(1,11)]
-        await asyncio.gather(*[extracting(url) for url in urls])
-        
+        result = await asyncio.gather(*[extracting(url) for url in urls])
+        data = []
+        for page in result:
+                for quote in page :
+                        data.append(quote)       
+        with open("quote.json","a") as f:
+                    json.dump(data,f,indent=4) 
+
 asyncio.run(all())
                 
