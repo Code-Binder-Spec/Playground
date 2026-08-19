03af71cbe2764e5503e807a3cd936f86006f3512
coder <adhimansoor.k@gmail.com>
2026-08-19 13:50:29 +0530

    trying to build a logic function

--- os_library_practicing/basics.py
+++ os_library_practicing/basics.py
@@ -1,4 +1,5 @@
 import os 
+
 def content_fetcher(path,depth=0):
     if os.path.exists(path):
                       items  = os.listdir(path)
@@ -6,11 +7,12 @@ def content_fetcher(path,depth=0):
                                     full_path = os.path.join(path,item)
                                     indent = "     "*depth
 
-                      if os.path.isdir(full_path):
-                            print(f"{indent} {item}")
-                            content_fetcher(full_path,depth=1)
-                      else :
-                            print(f"{indent} {item}")
+                                    if os.path.isdir(full_path):
+                                               print(f"{indent} {item}")
+                                               depth += 1
+                                               content_fetcher(full_path,depth)
+                                    else :
+                                                print(f"{indent} {item}")
     else :
             print("Path does not exist")
 
@@ -24,6 +26,20 @@ def specific_entry_exist_checker(path):
            print("Path does not exist")
 
        
+def fun_for_printing_specific_entry(items,type,path):
+        check = None
+        if type == "file":
+                check = False
+        else :
+                check = True
+        for item in items :
+                full_path = os.path.join(path,item)
+                if os.path.isdir(full_path):
+                        if check:
+                                print(item)
+                else :
+                        continue
+
 
 def specific_entry_lister(path,type):
      if os.path.exists(path):
@@ -34,7 +50,7 @@ def specific_entry_lister(path,type):
                           if os.path.isdir(full_path):
                                  continue
                           else :
-                                 print("item")
+                                 print(item)
             elif type == "folder":
                    for item in items:
                           full_path = os.path.join(path,item)
@@ -46,7 +62,12 @@ def specific_entry_lister(path,type):
                    
 
 cur_path = str(input("Enter current path : "))
+specific_entry_exist_checker(cur_path)
 content_fetcher(cur_path)
+print("content fetcher finished")
+specific_entry_lister(cur_path,"file")
+
+
 
 
       
