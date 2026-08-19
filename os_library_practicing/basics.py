4661ba0124352c661773db7406c867edfb4937e6
coder <adhimansoor.k@gmail.com>
2026-08-19 19:59:32 +0530

    builded all functions for creating my first ai agent

--- os_library_practicing/basics.py
+++ os_library_practicing/basics.py
@@ -9,8 +9,7 @@ def content_fetcher(path,depth=0):
 
                                     if os.path.isdir(full_path):
                                                print(f"{indent} {item}")
-                                               depth += 1
-                                               content_fetcher(full_path,depth)
+                                               content_fetcher(full_path,depth+1)
                                     else :
                                                 print(f"{indent} {item}")
     else :
@@ -27,45 +26,53 @@ def specific_entry_exist_checker(path):
 
        
 def fun_for_printing_specific_entry(items,type,path):
-        check = None
+        file = None
+        folder = None
         if type == "file":
-                check = False
+                file = True
         else :
-                check = True
+                folder = True
         for item in items :
                 full_path = os.path.join(path,item)
-                if os.path.isdir(full_path):
-                        if check:
-                                print(item)
-                else :
-                        continue
+                check = os.path.isdir(full_path)
+                if check and folder:
+                        print(item)
+                elif not check and file :
+                        print(item)
 
 
-def specific_entry_lister(path,type):
+def specific_entry_lister(path,type): 
      if os.path.exists(path):
             items = os.listdir(path)
-            if type == "file":
-                   for item in items:
-                          full_path = os.path.join(path,item)
-                          if os.path.isdir(full_path):
-                                 continue
-                          else :
-                                 print(item)
-            elif type == "folder":
-                   for item in items:
-                          full_path = os.path.join(path,item)
-                          if os.path.isdir(full_path):
-                                 print(item)
-                          else :
-                                 continue
-                   
-                   
+            fun_for_printing_specific_entry(items,type,path)
 
-cur_path = str(input("Enter current path : "))
+def specific_entry_adder(path,type):
+        file_type_list = ["txt", "md", "log", "csv", "env", "yaml", "yml", "py", "sh", "html"]
+        try :
+                       if type == "file":
+                                      file_name = str(input("Enter file name : "))
+                                      file_type = str(input("Enter file type [no dot needed].[only text based formats allowed] : "))
+                                      if file_type.lower() not in file_type_list:
+                                              raise ValueError("The file type you mentioned is invalid . check is it a text based format or it contains dot")
+                                      with open(f"{path}/{file_name}.{file_type.lower()}","w") as f:
+                                              pass
+                       elif type == "folder":
+                                      folder_name = str(input("Please enter the folder name : "))
+                                      full_path = os.path.join(path,folder_name)
+                                      os.mkdir(full_path)
+        except Exception as e :
+                print(f"error occured while creating . error : {e}")
+                                
+
+def entry_remover(path) :
+        os.remove(path)
+        
+                   
+cur_path = "/home/coder/coding/Validation-Database-Web-scarping"
 specific_entry_exist_checker(cur_path)
 content_fetcher(cur_path)
 print("content fetcher finished")
-specific_entry_lister(cur_path,"file")
+specific_entry_lister(cur_path,"folder")
 
 
 
